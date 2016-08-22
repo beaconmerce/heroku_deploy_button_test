41f13b7b63f34b793e901fd07a0f3c0838385485
Shrawan Lakhe <shrawanlakhey@gmail.com>
2016-08-22 16:01:55 +0545

    new commits

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app= express(),
+    path = require('path'),
+    exphbs = require('express-handlebars'),
+    port = process.env.PORT || 3000,
+    fs = require('fs'),
+    compression = require('compression');
+	
+    app.use(compression());
+
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization,x-access-token,Accept,Origin');
+
+    // Set cache control header to eliminate cookies from cache
+    res.setHeader('Cache-Control', 'no-cache="Set-Cookie, Set-Cookie2"');
+    next();
+});
+
+
+//set up the view engine-Handelbars
+app.set('views', path.join(__dirname, '/views'));
+
+var hbs = exphbs.create({
+    defaultLayout: 'main-layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: '.hbs'
+});
+app.engine('hbs', hbs.engine);
+app.engine('html',  hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next){
+	res.render('index', {
+                        title: "Nginx Heroku Setup"
+                    });
+});
+
+
+
+
+app.listen(port, function() {
+            console.log('server started on ' + port);
+        });
